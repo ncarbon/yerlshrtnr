94c492d900e5461144d78ce11e01675e12afedc6
Nataly Carbonell <natcarbonell97@gmail.com>
2018-11-10 21:53:37 -0500

    added redis mock

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,9 +1,10 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index');
-const redis = require('redis');
+const redis = require('redis-mock');
 const expect = chai.expect;
 const client = redis.createClient();
+const randomId = require('../randomId');
 
 chai.use(chaiHttp);
 
@@ -14,7 +15,7 @@ describe('index', () => {
         url = 'https://devcenter.heroku.com/articles/custom-domains#add-a-custom-domain-with-a-subdomain';
         ttl = 60;
         customId = 'mayo';
-
+        shortId = randomId();
         done();
     });
 
@@ -28,44 +29,50 @@ describe('index', () => {
     */
    describe('POST request for short url /api/shorturl', () => {
        it('should return the short url', (done) => {
+           client.set(shortId, url, (err, reply) => {
             chai.request(server)
                 .post('/api/shorturl')
                 .set('content-type', 'application/x-www-form-urlencoded')
                 .send({url: url})
                 .end((err, res) => {
                     expect(res).to.have.status(200)
-                     expect(res.body).to.have.nested.property('shortURL');
+                    expect(res.body).to.have.nested.property('shortURL');
                     done();
                 });
+           });
        });
 
        it('should return URL with requested custom id', (done) => {
-            chai.request(server)
-            .post('/api/shorturl')
-            .set('content-type', 'application/x-www-form-urlencoded')
-            .send({url: url, customId: customId})
-            .end((err, res) => {
-                console.log(res.body)
-                expect(res).to.have.status(200)
-                expect(res.body).to.have.nested.property('shortURL');
-                expect(res.body.shortURL).to.match(new RegExp('http://localhost:3100/' + customId));
-                done();
-            });
+           client.set(customId, url, (err, reply) => {
+                chai.request(server)
+                .post('/api/shorturl')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({url: url, customId: customId})
+                .end((err, res) => {
+                    console.log(res.body)
+                    expect(res).to.have.status(200)
+                    expect(res.body).to.have.nested.property('shortURL');
+                    expect(res.body.shortURL).to.match(new RegExp('http://localhost:3100/' + customId));
+                    done();
+                });
+           })
        });
 
        it('should return URL with expiration', (done) => {
-        chai.request(server)
-        .post('/api/shorturl')
-        .set('content-type', 'application/x-www-form-urlencoded')
-        .send({url: url, ttl: ttl})
-        .end((err, res) => {
-            console.log(res.body)
-            expect(res).to.have.status(200)
-            expect(res.body).to.have.nested.property('shortURL');
-            expect(res.body.expires).to.be.true;
-            expect(parseInt(res.body.ttl)).to.be.equal(ttl);
-            done();
+           client.set(shortId, url, 'EX', ttl, (err, reply) => {
+            chai.request(server)
+                .post('/api/shorturl')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({url: url, ttl: ttl})
+                .end((err, res) => {
+                    console.log(res.body)
+                    expect(res).to.have.status(200)
+                    expect(res.body).to.have.nested.property('shortURL');
+                    expect(res.body.expires).to.be.true;
+                    expect(parseInt(res.body.ttl)).to.be.equal(ttl);
+                    done();
+                });
+           });
         });
-   });
    });
 });
